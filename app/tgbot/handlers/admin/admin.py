baea423eb40d59d484f0c3ba135d8f875c3bf91c
Hum4noid <hum4noid@yandex.ru>
2023-02-08 19:48:55 +0300

    fix(merge): fix merge errors in imports, change function args

--- app/tgbot/handlers/admin/admin.py
+++ app/tgbot/handlers/admin/admin.py
@@ -1,13 +1,14 @@
 import logging
 import asyncio
 from aiogram.exceptions import TelegramBadRequest
-from aiogram import Router
+from aiogram import Router, Bot
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode
 
 from infrastructure.database.repositories.admin import AdminRepo
 from infrastructure.database.repositories.bot import BotRepo
+from infrastructure.database.repositories.user import UserReader
 from tgbot.states.admin.menu import AdminMenu
 
 logger = logging.getLogger(__name__)
@@ -25,62 +26,62 @@ async def sending_messages_to_all_users(event: Message, user_reader: UserReader,
     logger.info(f'User {event.from_user.id} sent message to all users')
 
 
-async def admin_start(m: Message, dialog_manager: DialogManager):
+async def admin_start(event: Message, dialog_manager: DialogManager):
     await dialog_manager.start(AdminMenu.admin_menu, mode=StartMode.RESET_STACK)
 
 
-async def start_maintenance(m: Message, bot_repo: BotRepo):
+async def start_maintenance(event: Message, bot_repo: BotRepo):
     await bot_repo.set_maintenance()
-    await m.reply('♻️ Режим обслуживания включен')
-    logger.info(f'User {m.from_user.id} turned on maintenance mode')
+    await event.reply('♻️ Режим обслуживания включен')
+    logger.info(f'User {event.from_user.id} turned on maintenance mode')
 
 
-async def stop_maintenance(m: Message, bot_repo: BotRepo):
+async def stop_maintenance(event: Message, bot_repo: BotRepo):
     await bot_repo.disable_maintenance()
-    await m.reply('✅ Режим обслуживания выключен')
-    logger.info(f'User {m.from_user.id} turned off maintenance mode')
+    await event.reply('✅ Режим обслуживания выключен')
+    logger.info(f'User {event.from_user.id} turned off maintenance mode')
 
 
-async def add_admin(m: Message, command: CommandObject, admin_repo: AdminRepo):
+async def add_admin(event: Message, command: CommandObject, admin_repo: AdminRepo):
     new_admin = command.args
     try:
         admin_id = int(new_admin)
     except TypeError:
-        await m.reply('✏️ Пожалуйста, укажите Telegram ID администратора')
+        await event.reply('✏️ Пожалуйста, укажите Telegram ID администратора')
         return
     except ValueError:
-        await m.reply('❌ Telegram ID администратора должен быть числом!')
+        await event.reply('❌ Telegram ID администратора должен быть числом!')
         return
     result = await admin_repo.add_admin(admin_id)
     if result:
-        logger.info(f'User {m.from_user.id} added admin {admin_id}')
-        await m.reply(f'✅ Пользователь {admin_id} добавлен в список админов')
+        logger.info(f'User {event.from_user.id} added admin {admin_id}')
+        await event.reply(f'✅ Пользователь {admin_id} добавлен в список админов')
     else:
-        await m.reply(f'❗ Пользователь {admin_id} не найден')
-        logger.info(f'User {m.from_user.id} tried to add admin {admin_id}, but he is not found')
+        await event.reply(f'❗ Пользователь {admin_id} не найден')
+        logger.info(f'User {event.from_user.id} tried to add admin {admin_id}, but he is not found')
 
 
-async def ban_user(m: Message, command: CommandObject, admin_repo: AdminRepo):
+async def ban_user(event: Message, command: CommandObject, admin_repo: AdminRepo):
     user_id = command.args
     try:
         user_id = int(user_id)
     except TypeError:
-        await m.reply('Пожалуйста, укажите Telegram ID пользователя')
+        await event.reply('Пожалуйста, укажите Telegram ID пользователя')
         return
     except ValueError:
-        await m.reply('Telegram ID пользователя должен быть числом!')
+        await event.reply('Telegram ID пользователя должен быть числом!')
         return
     result = await admin_repo.ban_user(user_id)
     if result:
-        logger.info(f'User {m.from_user.id} banned {user_id}')
-        await m.reply(f'🚫 Пользователь {user_id} заблокирован')
+        logger.info(f'User {event.from_user.id} banned {user_id}')
+        await event.reply(f'🚫 Пользователь {user_id} заблокирован')
     else:
-        await m.reply(f'❗ Пользователь {user_id} не найден')
-        logger.info(f'User {m.from_user.id} tried to ban {user_id}, but he is not found')
+        await event.reply(f'❗ Пользователь {user_id} не найден')
+        logger.info(f'User {event.from_user.id} tried to ban {user_id}, but he is not found')
 
 
-async def get_file_id(m: Message):
-    await m.reply(m.document.file_id)
+async def get_file_id(event: Message):
+    await event.reply(event.document.file_id)
 
 
 def register_admin_router(router: Router):

c6388b7bae6496cf706c2e36c7e7fee7dba16702
Hum4noid <hum4noid@yandex.ru>
2023-02-08 14:04:19 +0300

    refactor: remove validators

--- app/configreader.py
+++ app/configreader.py
@@ -17,18 +17,6 @@ class Config(BaseSettings):
     environment: Optional[str]
     service_name: Optional[str]
 
-    @validator("bot_fsm_storage")
-    def validate_bot_fsm_storage(cls, v):
-        if v not in ("memory", "redis"):
-            raise ValueError("Incorrect 'bot_fsm_storage' value. Must be one of: memory, redis")
-        return v
-
-    # @validator("redis_dsn")
-    # def validate_redis_dsn(cls, v, values):
-    #     if values["bot_fsm_storage"] == "redis" and not v:
-    #         raise ValueError("Redis DSN string is missing!")
-    #     return v
-
     @validator("webhook_path")
     def validate_webhook_path(cls, v, values):
         if values["webhook_domain"] and not v:
